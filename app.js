7f8bf0707e8234c071122dc928a7869210da5641
Sheroo Bhagia <sheroo@sb-mac.lan>
2022-02-19 13:25:27 -0600

    code from Canvas

--- app.js
+++ app.js
@@ -1,29 +1,18 @@
 const fs = require('fs');
+const generatePage = require('./src/page-template');
 
-const generatePage = require('./src/page-template.js');
+const profileDataArgs = process.argv.slice(2);
 
-const profileDataArgs = process.argv.slice(2, process.argv.length);
+console.log(profileDataArgs);
 
-const [name1, github] = profileDataArgs;
+const [name, github] = profileDataArgs;
 
+console.log(name, github);
 
+const pageHTML = generatePage(name, github);
 
-fs.writeFile("./index.html", generatePage(name1, github), err => {
-    if (err) throw new Error(err);
-    console.log("Portfolio complete! Check out index.html to see the output!");
+fs.writeFile('./index.html', pageHTML, err => {
+  if (err) throw err;
+
+  console.log('Portfolio complete! Check out index.html to see the output!');
 });
-
-
-// const printProfileData = profileDataArr => {
-//   // This...
-//   for (let i = 0; i < profileDataArr.length; i += 1) {
-//     console.log(profileDataArr[i]);
-//   }
-
-//   console.log('================');
-
-//   // Is the same as this...
-//   profileDataArr.forEach(profileItem => console.log(profileItem));
-// };
-
-// printProfileData(profileDataArgs);
